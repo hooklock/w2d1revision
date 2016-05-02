41028bab7279f3ad2436d9040968568d3c7ab12d
Adam Pinner <hooklock@gmail.com>
2016-05-02 20:07:50 +0100

    initial commit of files

---
+++ specs/bears_spec.rb
@@ -0,0 +1,12 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../bears')
+require_relative('../river')
+require_relative('../fish')
+
+class TestBear <MiniTest::Test
+
+
+
+end
+      
